653146cb26ba0e4af9b7450fb2eafa9b31085a77
WWWCourses <ivawebcourses@gmail.com>
2017-09-16 13:36:58 +0300

    fixes

---
+++ .bin/load_chrome.sh
@@ -0,0 +1,6 @@
+# Profile 1 = WD:ProgressBG
+# Profile 2 = IT-AcademyCourse
+# Profile 3 = phoneiep
+# Profile 4 = JSProgressBG
+# Profile 5 = JavelinJS
+google-chrome  --profile-directory="Profile 2" https://mail.google.com http://wwwcourses.github.io/IT-Academy-Front-End-Dev/ &
