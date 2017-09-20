95cd7e60c1dc183ef21bfecd0811fee86be593a8
WWWCourses <ivawebcourses@gmail.com>
2017-09-20 15:30:55 +0300

    reveal.js disclaimer added

--- outfit/js/slides.js
+++ outfit/js/slides.js
@@ -1,6 +1,6 @@
 function themeSwitcher(themeName){
     alert("themeSwitcher ON");
-    document.getElementById('theme').setAttribute('href','/IT-Academy-Front-End-Dev/lib/reveal.js/css/theme/'+themeName+'.css');
+    document.getElementById('theme').setAttribute('href','/ProgressBG-Front-End-Dev/lib/reveal.js/css/theme/'+themeName+'.css');
 }
 function  PrettyPreCode(){
     var codeNodes = document.querySelectorAll('pre>code');
@@ -8,24 +8,41 @@ function  PrettyPreCode(){
     for (var i = 0; i < codeNodes.length; i++)
     {
         var content = codeNodes[i].innerHTML;
-        // console.log("content:", content);
 
-        var lines = content.split('\n');
-        // console.log("lines:", lines);
+        // get indent
+        var indent =  content.match(/^\n*(\s*)/)[1];
 
-        if ( !lines[1] ){
-            continue;
-        }
-        var tab_index = lines[1].search(/\S/);
-        // console.log("tab_index", tab_index);
+        // remove indent from all lines
+        var indentRE = new RegExp("^" + indent, "gm");
+        content = content.replace(indentRE, "");
 
-        var contentNew = '';
-        for ( var j=0; j<lines.length; j++ ){
-            lines[j] = lines[j].substring(tab_index-1);
-            contentNew += lines[j]+'\n';
-        }
+        // clean empty lines on start/end
+        content = content.replace(/^\s*/,"");
+        content = content.replace(/\s*$/, "");
 
-        codeNodes[i].innerHTML = contentNew;
+        codeNodes[i].innerHTML = content;
+        codeNodes[i].style.overflow="auto";
+    }
+}
+function  PrettyPreCode(){
+    var codeNodes = document.querySelectorAll('pre>code');
+
+    for (var i = 0; i < codeNodes.length; i++)
+    {
+        var content = codeNodes[i].innerHTML;
+
+        // get indent
+        var indent =  content.match(/^\n*(\s*)/)[1];
+
+        // remove indent from all lines
+        var indentRE = new RegExp("^" + indent, "gm");
+        content = content.replace(indentRE, "");
+
+        // clean empty lines on start/end
+        content = content.replace(/^\s*/,"");
+        content = content.replace(/\s*$/, "");
+
+        codeNodes[i].innerHTML = content;
         codeNodes[i].style.overflow="auto";
     }
 }
