d9e79ace245ff7fdf7f9f139cf6d1cb576f76522
WWWCourses <ivawebcourses@gmail.com>
2017-11-05 23:53:30 +0200

    slides  update

---
+++ .bin/live-server.sh
@@ -0,0 +1,5 @@
+nohup live-server --port=1111 --open=/IT-Academy-Front-End-Dev/pages/agenda/beginners.html \
+--watch=/IT-Academy-Front-End-Dev   \
+/home/nemsys/projects/www/wwwcourses.github.io &
+
+echo 'live-server started on port=1111 for IT-Academy-Front-End-Dev'
